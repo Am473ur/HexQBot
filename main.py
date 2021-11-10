da1ceb70dc9bc023c2b4b84f89ecd3931a790810
Am473ur <1175078221@qq.com>
2021-11-10 19:30:13 +0800

    Rename files

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-from HttpAPI import *
-from BaiduImg import *
-from common_reply import *
+from http_api import *
+
 while True:
     recv_json = receive_message()
     if recv_json == None: continue
-    print(recv_json)
     if recv_json["post_type"] == "message":
-        if recv_json["sub_type"] != "friend":
-            send_msg("请先加我为好友呀~", recv_json["user_id"], "private")
-        else:
-            send_msg(reply(recv_json), recv_json["user_id"], "private")
+        pass
 
 
