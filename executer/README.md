0704b0d38029fc6fd89d71d3b06641dcb2b04979
Am473ur <1175078221@qq.com>
2021-11-11 21:30:40 +0800

    Fix Dockerfile

--- executer/README.md
+++ executer/README.md
@@ -2,4 +2,7 @@
 
 ## Run in docker
 
-
+```
+docker build . -t executer
+docker run --name executer -d -p 20000:20000 executer
+```
---
+++ executer/executer.py
@@ -0,0 +1,45 @@
+# !/usr/bin/env python3
+from fastapi import FastAPI
+from time import ctime
+
+app = FastAPI()
+
+block_words = ["class", "import", "eval", "exec", "sys", "globals","os", "_", "builtins","getattr", "pow"]
+
+def logger(msg, tag="[+]"):
+    with open("executer.log", "a") as f:
+        f.write(f"{ctime()}: {tag} {msg}\n")
+
+def finder(keyword, msg):
+    for i in keyword:
+        if i not in msg: return False
+    return True
+
+def block_word(msg):
+    for word in block_words:
+        if finder(word, msg): return False
+    if "**" in msg:           return False
+    return True
+
+def eval_msg(code):
+    try:
+        if block_word(code): 
+            temp = eval(code)   
+        else:               return [False, 0]
+        if temp != None:    return [True, str(temp)]
+        else:               return [False, 1]
+    except:                 return [False, 2]
+
+@app.get("/executer")
+def executer(code):
+    logger(code)
+    result = eval_msg(code)
+    if result[0]:
+        logger(result[1], "[~]")
+        return {"result":1, "data":result[1]}
+    return  {"result":1000+result[1], "data":"error!!"}
+
+if __name__ == '__main__':
+    import uvicorn
+    uvicorn.run(app='executer:app', host="0.0.0.0", port=20000)
+
