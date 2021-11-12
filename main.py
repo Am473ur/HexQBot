bc336f315e489f8ba62d458913fb0da1bea8ff40
Am473ur <1175078221@qq.com>
2021-11-12 16:57:23 +0800

    test

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ from http_api import *
 
 while True:
     recv_json = receive_message()
-    if recv_json == None: continue
-    if recv_json["post_type"] == "message":
-        pass
+    if not recv_json: continue
+    
+
 
 
